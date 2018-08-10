9b41ec86bbad652771e68c55feb310cc55f86aa5
ninjaPixel <matthew@ninjaPixel.eu>
2018-08-10 09:20:38 +0100

    v 0.0.4

--- src/DeferredComponent.jsx
+++ src/DeferredComponent.jsx
@@ -5,19 +5,22 @@ import PropTypes from 'prop-types';
 class DeferredComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { Component: null, loading: true };
+    this.state = {Component: null, loading: true};
   }
+
   componentDidMount() {
     this.loadComponent();
   }
-  async loadComponent() {
-    const Component = await this.props.importFunction();
-    this.setState({ loading: false, Component: Component.default });
+
+  loadComponent() {
+    this.props.importFunction().then((Component) => {
+      this.setState({loading: false, Component: Component.default});
+    });
   }
 
   render() {
     const props = this.props;
-    const { loading, Component } = this.state;
+    const {loading, Component} = this.state;
     if (loading) {
       return this.props.loadingComponent || null
     }
